663ff133cf34962f890982589f280daf38c07bfd
Tony Liu <mrtoniliu@gmail.com>
2021-04-17 16:07:04 +0100

    Update with basic header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import './App.css';
-import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
-import ShopPage from './pages/shop/shoppage.component';
+import React from "react";
+import "./App.css";
+import HomePage from "./pages/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+import ShopPage from "./pages/shop/shoppage.component";
+import Header from "./components/header/header.component";
 
 const HatsPage = () => (
   <div>
@@ -13,10 +14,11 @@ const HatsPage = () => (
 function App() {
   return (
     <div>
-    <Switch>
-      <Route exact path='/' component={HomePage}/>
-      <Route path='/shop' component={ShopPage}/>
-    </Switch>
+      <Header/>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/shop" component={ShopPage} />
+      </Switch>
     </div>
   );
 }
