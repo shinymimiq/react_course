caed1577b2af8f93d268b82fe53053126c8be1d1
Tony Liu <mrtoniliu@gmail.com>
2021-04-18 21:50:54 +0100

    Update for Google Sign in

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import { auth } from '../../firebase/firebase.util';
+
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
+import SignIn from '../sign-in/sign-in.component';
 
-const Header = () => {
+const Header = ({ currentUser }) => {
   return (
     <div className='header'>
       <Link className='logo-container' to="/" >
@@ -17,6 +20,12 @@ const Header = () => {
         <Link className='option' to='/contact'>
           CONTACT
         </Link>
+        {
+          currentUser ? 
+          <div className='option' onClick={ () => auth.signOut() }>SIGN OUT</div>
+          :
+          <Link className='option' to='/signin'>SIGN IN</Link>
+        }
       </div>
     </div>
   )
