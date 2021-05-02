e14f8114d6e2a51132427df6a48477863c55028e
Shiny Mimikyu <mrtoniliu@gmail.com>
2021-05-02 19:05:37 +0100

    Redux setup

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ import App from "./App";
 import * as serviceWorkerRegistration from "./serviceWorkerRegistration";
 import reportWebVitals from "./reportWebVitals";
 
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 
+import store from "./redux/store";
+
 ReactDOM.render(
-  <React.StrictMode>
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+  </Provider>,
   document.getElementById("root")
 );
 
