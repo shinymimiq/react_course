2545abdda2892ac365890122a831ae72d74c1ac9
Shiny Mimikyu <mrtoniliu@gmail.com>
2021-05-03 17:24:50 +0100

    Update redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { connect } from "react-redux";
+
 import { auth } from "../../firebase/firebase.util";
 
 import "./header.styles.scss";
@@ -34,4 +36,8 @@ const Header = ({ currentUser }) => {
   );
 };
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser,
+});
+
+export default connect(mapStateToProps)(Header);
