143dd3d0f95fbd805133043954ffe393b8ee6bb7
Shiny Mimikyu <mrtoniliu@gmail.com>
2021-05-07 23:12:13 +0100

    Impelemented Checkout page, checkout item

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
-import HomePage from "./pages/homepage/homepage.component";
 import { Switch, Route, Redirect } from "react-router-dom";
-import ShopPage from "./pages/shop/shoppage.component";
 import Header from "./components/header/header.component";
+
+import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shoppage.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
 import { connect } from "react-redux";
 import { createStructuredSelector } from "reselect";
@@ -46,6 +48,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
           <Route
             path="/signin"
             render={() =>
